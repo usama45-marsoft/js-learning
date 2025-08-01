79804b777aa70dce0ad2ae3e0804d312760c96ac
usama45-marsoft <osamamarsoft@gmail.com>
2025-08-02 01:46:00 +0500

    variable class changes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,20 @@
+const accountId = 1234
+let accountEmail = "usama@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountStates;
+
+// accountId = 2
+
+
+console.log(accountId);
+
+
+accountEmail = "admin@gmail.com"
+accountPassword = "2121212"
+accountCity = "karachi"
+
+
+// console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountStates])
